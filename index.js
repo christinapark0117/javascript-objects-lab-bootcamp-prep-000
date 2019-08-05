346d9b49568c514adab09c9c0cf1d4e2797a3519
Christina Park <christinapark0117@gmail.com>
2019-08-05 21:16:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newRecipes = Object.assign({},object)
   delete newRecipes.key
-  return object
+  return newRecipes
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
